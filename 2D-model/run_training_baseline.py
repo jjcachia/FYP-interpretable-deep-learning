d5efd40a3ddcc2055b70b00816499fd407381f4b
jjcachia <jeremy.cachia23@imperial.ac.uk>
2024-08-29 13:14:14 +0200

    Bugfix 4

--- 2D-model/run_training_baseline.py
+++ 2D-model/run_training_baseline.py
@@ -26,6 +26,7 @@ def parse_args():
     parser.add_argument('--backbone', type=str, default='denseNet121', help='Feature Extractor Backbone to use')
     parser.add_argument('--model', type=str, default='base', help='Model to train')
     parser.add_argument('--experiment_run', type=str, required=True, help='Identifier for the experiment run')
+    parser.add_argument('--device', type=str, default='0', help='GPU device to use')
     parser.add_argument('--weights', type=str, default='DEFAULT', help='Weights to use for the backbone model')
     
     parser.add_argument('--img_channels', type=int, default=IMG_CHANNELS, help='Number of channels in the input image')
@@ -69,7 +70,8 @@ def main():
     else:
         print("CUDA is not available. Only CPU is available.")
     
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:' + args.device) 
     print(f"Using device: {device}")
 
     ###############################################################################################################
