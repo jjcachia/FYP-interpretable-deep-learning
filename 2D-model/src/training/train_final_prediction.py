0fcebc39302d22788d437480dc4ec54ee096020f
jjcachia <jeremy.cachia23@imperial.ac.uk>
2024-08-31 04:01:01 +0200

    Bugfix

--- 2D-model/src/training/train_final_prediction.py
+++ 2D-model/src/training/train_final_prediction.py
@@ -28,9 +28,10 @@ def _train_or_test(model, data_loader, optimizer, device, is_train=True):
             # Forward pass
             outputs = model(X)
             
+            bweight_pred = bweight_pred * slice_weight
+            
             # Compute loss
             loss = torch.nn.functional.binary_cross_entropy(outputs, y, weight = bweight_pred)
-            loss = loss * slice_weight
             total_loss += loss.item()
             
             # Collect data for statistics
