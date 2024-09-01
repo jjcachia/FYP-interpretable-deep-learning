264382872eaafb9a5907a8be813a715908873d4d
jjcachia <jeremy.cachia23@imperial.ac.uk>
2024-09-01 14:49:25 +0200

    bugfix

--- 2D-model/src/loaders/_2D/dataloader.py
+++ 2D-model/src/loaders/_2D/dataloader.py
@@ -126,7 +126,7 @@ class LIDCDataset(Dataset):
                 continue
             label = characteristics.iloc[idx, char_idx]
             label_chars.append(label)
-            bweight_chars.append(self.char_weights.iloc[:, char_idx])
+            bweight_chars.append(self.char_weights.iloc[:, char_idx].values)
             
         # Extract the final prediction label and binary weight
         final_pred_label = self.labels['Malignancy'].iloc[idx]
