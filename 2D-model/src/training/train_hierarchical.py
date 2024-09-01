da1c9df5f586b3fcb1486405d8eb1c4f310bd09f
jjcachia <jeremy.cachia23@imperial.ac.uk>
2024-09-01 17:35:44 +0200

    bugfix

--- 2D-model/src/training/train_hierarchical.py
+++ 2D-model/src/training/train_hierarchical.py
@@ -61,13 +61,16 @@ def _train_or_test(model, data_loader, optimizer, device, is_train=True, task_we
             
             loss = 0
             for i, (task_output, target, bweight_char) in enumerate(zip(task_outputs, targets, bweights_chars)):
-                bweight_char = bweight_char[0] # Get the first element of the batch
+                bweight_char = bweight_char[0][target] # Get the first element of the batch
+                print(bweight_char)
                 bweight_char = bweight_char * slice_weight
+                print(bweight_char)
                 
                 # Compute loss for each task
                 # task_loss = torch.nn.functional.cross_entropy(task_output, target, weight=bweight_char)
                 task_loss = torch.nn.functional.cross_entropy(task_output, target, reduction='none')
                 task_loss = (task_loss * bweight_char).mean()
+                
                 # Multiply the loss by the task weight
                 if task_weights:
                     task_loss =  task_loss * task_weights[i]
