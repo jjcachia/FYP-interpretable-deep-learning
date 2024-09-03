b4754153545f4b8473938355cd6f73f290f7c068
jjcachia <jeremy.cachia23@imperial.ac.uk>
2024-09-03 13:24:58 +0200

    bugfix

--- 2D-model/src/loaders/_2D/dataloader.py
+++ 2D-model/src/loaders/_2D/dataloader.py
@@ -99,12 +99,12 @@ class LIDCDataset(Dataset):
         # Load the image
         path = self.labels['image_dir'].iloc[idx]
         array = np.load(path)
-        # array = np.expand_dims(array, axis=0) # Convert to 1-channel image
-        # array = np.repeat(array, 3, axis=0) # Convert to 3-channel image
-        # image = torch.from_numpy(array)        
-        image = Image.fromarray(array)
-        image = transforms.Grayscale(num_output_channels=3)(image)
-        image = transforms.ToTensor()(image)
+        array = np.expand_dims(array, axis=0) # Convert to 1-channel image
+        array = np.repeat(array, 3, axis=0) # Convert to 3-channel image
+        image = torch.from_numpy(array)        
+        # image = Image.fromarray(array)
+        # image = transforms.Grayscale(num_output_channels=3)(image)
+        # image = transforms.ToTensor()(image)
         
         # Get the weight for the slice
         num_slices = self.labels['total_slices'].iloc[idx]
